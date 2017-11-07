5a4db799a1c2da2e976bf0031c2c452d691a7115
JodeQ <jodeq@jeucodeinformatique.fr>
2017-11-07 11:34:34 +0100

    Découpage du code

--- index.php
+++ index.php
@@ -1,85 +1,48 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-  <title>Exemple</title>
+  <!-- Insérer le css ici -->
+  <link rel="stylesheet" type="text/css" href="style.css">
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 <body>
 
   <?php
-    $nom = 'Dupont';
-    $prénom = 'Jean';
-
-    echo "Bonjour " . $nom . " " . $prénom;
-
-    echo "<br/>";
-    $age = 33;
-
-    echo "J'ai $age ans";
-    echo 'J\'ai $age ans';
-
-    echo "<br>";
-
-    if ($age > 18 && $prénom == "Jean") {
-        echo "Majeur";
-    } elseif($age >= 15) {
-    	echo "Ado";
-    } else {
-    	echo "Enfant";
-    }
-
-    $age2 = 33;
-    echo"<br>";
-    echo $age <=> $age2;
-    echo"<br>";
-    echo 1 <=> 2;
-    echo"<br>";
-    echo 2 <=> 1;
-    echo"<br>";
-    // Déclaration de variables
-    $a = null;
-
-    $c = 5;
-    $b;
-    $d;
-
-    echo $a ?? $b ?? $c ?? $d;
-
-    echo "<br>";
-    // L'arithmétique :
-    $a = 5;
-    $b = $a + 5;
-    echo "b vaut $b et a vaut $a";
-    echo "<br>";
-    $b = $a += 5; // $a = $a + 5
-    echo "b vaut $b et a vaut $a";
-
-    $a++;  // $a = $a + 1
-    echo "a vaut $a <br>";
-
-    ++$a;  // $a = $a + 1
-    echo "a vaut $a <br>";
-
-
-    // différence :
-    echo "avant a vaut " . ++$a;
-    echo " après a vaut " . $a;
-    echo "<br>";
-    echo "avant a vaut " . $a++;
-    echo " après a vaut " . $a;
-    echo "<pre>";
-    print_r ($a);
-    echo"</pre>";
-    echo "<pre>";
-    var_dump($a);
-    echo "ce code n'est pas interprété\n\ndu tout";
-    echo "</pre>";
-
-    $array = ["couleur" => "rouge", "taille" => 1.70];
-    echo "<pre>";
-    var_dump($array);
-    echo "</pre>";
-die ("fini");
+    require("pokemon.php");
   ?>
+
+  <form>
+    <fieldset>
+      <legend>Pokemon 1 :
+        <select id="pokemon1" name="pokemon1" <?php echo isset($form_error['pokemon1']) ? 'class="error"' : ''; ?>>
+          <?php
+            foreach($pokemons as $pokemon => $stats) {
+              echo '<option value="' . $pokemon . '">' . $pokemon . '</option>';
+            }
+          ?>
+        </select>
+      </legend>
+      <div>Points de vie : <input type="test" name="pv_pokemon1" value="<?php echo $pikachu['pv']; ?>" <?php echo isset($form_error['pv_pokemon1']) ? 'class="error"' : ''; ?> /></div>
+      <div>Points de défense : <input type="test" name="defense_pokemon1" value="<?php echo $pikachu['defense']; ?>" <?php echo isset($form_error['defense_pokemon1']) ? 'class="error"' : ''; ?> /></div>
+      <div>Points d'attaque : <input type="test" name="attaque_pokemon1" value="<?php echo $pikachu['attaque']; ?>" <?php echo isset($form_error['attaque_pokemon1']) ? 'class="error"' : ''; ?> /></div>
+    </fieldset>
+    <fieldset>
+      <legend>Pokemon 2 :
+        <select id="pokemon2" name="pokemon2" <?php echo isset($form_error['pokemon2']) ? 'class="error"' : ''; ?>>
+          <?php
+            foreach($pokemons as $pokemon => $stats) {
+              echo '<option value="' . $pokemon . '" ' . ($pokemon == 'Bulbizarre' ? 'selected' : '') . '>' . $pokemon . '</option>';
+            }
+          ?>
+        </select>
+      </legend>
+      <div>Points de vie : <input type="test" name="pv_pokemon2" value="<?php echo $bulbizarre['pv']; ?>" <?php echo isset($form_error['pv_pokemon2']) ? 'class="error"' : ''; ?> /></div>
+      <div>Points de défense : <input type="test" name="defense_pokemon2" value="<?php echo $bulbizarre['defense']; ?>" <?php echo isset($form_error['defense_pokemon2']) ? 'class="error"' : ''; ?> /></div>
+      <div>Points d'attaque : <input type="test" name="attaque_pokemon2" value="<?php echo $bulbizarre['attaque']; ?>" <?php echo isset($form_error['attaque_pokemon2']) ? 'class="error"' : ''; ?> /></div>
+    </fieldset>
+    <button type="submit">Combattez !</button>
+  </form>
 
 </body>
 </html>
--- pokemon.php
+++ pokemon.php
@@ -1,13 +1,9 @@
-<html>
-<head>
-  <!-- Insérer le css ici -->
-  <link rel="stylesheet" type="text/css" href="style.css">
-
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-</head>
-<body>
-
 <?php
+/**
+ * Bienvenue dans ce module PHP
+ * Nous allons travailler à la réalisation d'un pokedex
+ */
+
 // Initialisation des variables
 
 // Mes pokemons
@@ -90,46 +86,6 @@ foreach($_GET as $input => $value) {
   }
 }
 
-?>
-
-  <form>
-    <fieldset>
-      <legend>Pokemon 1 :
-        <select id="pokemon1" name="pokemon1" <?php echo isset($form_error['pokemon1']) ? 'class="error"' : ''; ?>>
-          <?php
-            foreach($pokemons as $pokemon => $stats) {
-              echo '<option value="' . $pokemon . '">' . $pokemon . '</option>';
-            }
-          ?>
-        </select>
-      </legend>
-      <div>Points de vie : <input type="test" name="pv_pokemon1" value="<?php echo $pikachu['pv']; ?>" <?php echo isset($form_error['pv_pokemon1']) ? 'class="error"' : ''; ?> /></div>
-      <div>Points de défense : <input type="test" name="defense_pokemon1" value="<?php echo $pikachu['defense']; ?>" <?php echo isset($form_error['defense_pokemon1']) ? 'class="error"' : ''; ?> /></div>
-      <div>Points d'attaque : <input type="test" name="attaque_pokemon1" value="<?php echo $pikachu['attaque']; ?>" <?php echo isset($form_error['attaque_pokemon1']) ? 'class="error"' : ''; ?> /></div>
-    </fieldset>
-    <fieldset>
-      <legend>Pokemon 2 :
-        <select id="pokemon2" name="pokemon2" <?php echo isset($form_error['pokemon2']) ? 'class="error"' : ''; ?>>
-          <?php
-            foreach($pokemons as $pokemon => $stats) {
-              echo '<option value="' . $pokemon . '" ' . ($pokemon == 'Bulbizarre' ? 'selected' : '') . '>' . $pokemon . '</option>';
-            }
-          ?>
-        </select>
-      </legend>
-      <div>Points de vie : <input type="test" name="pv_pokemon2" value="<?php echo $bulbizarre['pv']; ?>" <?php echo isset($form_error['pv_pokemon2']) ? 'class="error"' : ''; ?> /></div>
-      <div>Points de défense : <input type="test" name="defense_pokemon2" value="<?php echo $bulbizarre['defense']; ?>" <?php echo isset($form_error['defense_pokemon2']) ? 'class="error"' : ''; ?> /></div>
-      <div>Points d'attaque : <input type="test" name="attaque_pokemon2" value="<?php echo $bulbizarre['attaque']; ?>" <?php echo isset($form_error['attaque_pokemon2']) ? 'class="error"' : ''; ?> /></div>
-    </fieldset>
-    <button type="submit">Combattez !</button>
-  </form>
-
-<?php
-/**
- * Bienvenue dans ce module PHP
- * Nous allons travailler à la réalisation d'un pokedex
- */
-
 // Vérifions les informations
 /*echo "<pre>";
 var_dump($_GET);
@@ -140,7 +96,7 @@ if (count($form_error) > 0)
 
 if (count($_GET) == 0) {
   echo "<h2>Veuillez sélectionner vos pokemons et lancez le combat</h2>";
-  exit;
+  return;
 }
 
 $tour = 0;
@@ -224,7 +180,3 @@ $pv_baie_noire = 30;
 // Pikachu mange une baie noire
 
 ?>
-
-
-</body>
-</html>
