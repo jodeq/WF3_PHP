d7e7655eb913275c102207326add3756c6811fd9
JodeQ <jodeq@jeucodeinformatique.fr>
2017-11-08 10:39:21 +0100

    BDD !

--- test/db.php
+++ test/db.php
@@ -2,6 +2,25 @@
 /*
  * Script de test pour la connexion Base de donnée : BDD
  */
+echo "<pre>";
+$db = new PDO('mysql:host=localhost;dbname=pokemon', 'root', '');
+echo "DB : ";
+var_dump($db);
+
+$query = $db->query('SELECT * FROM pokedex');
+$result = $query->fetch();
+echo "result : ";
+var_dump($result);
+
+$query = $db->query("INSERT INTO pokedex(nom_proprietaire) VALUES('Sacha')");
+$query->execute();
+
+$query = $db->query('SELECT * FROM pokedex');
+$result = $query->fetch();
+echo "result : ";
+var_dump($result);
+
+echo "</pre>";
 
 
 ?>
